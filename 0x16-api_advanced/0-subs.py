384661ef46a54611c2c1fd31820253585ea2689e
farah-halim <level2bis2@gmail.com>
2023-11-07 11:41:07 -0800

    task 0&1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,14 +5,12 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Gets the number of subscribers for a given subreddit"""
-    url = "http://reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    lin = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(lin, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    dic = response.json()
-    if "data" not in dic:
-        return 0
-    if "subscribers"not in dic.get("data"):
-        return 0
-    return response.json()["data"]["subscribers"]
+    results = response.json().get("data")
+    return results.get("subscribers")
